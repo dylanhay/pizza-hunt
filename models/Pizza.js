ad48bee57450311f5218c24cf1708a8a7f058c33
dchay <dylanhay@rogers.com>
2022-09-11 15:57:09 -0700

    reply routes tested

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,4 +1,4 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model, Types } = require("mongoose");
 const dateFormat = require('../utils/dateFormat');
 
 const PizzaSchema = new Schema(
@@ -35,9 +35,8 @@ const PizzaSchema = new Schema(
   }
 );
 
-// get total count of comments and replies on retrieval
-PizzaSchema.virtual("commentCount").get(function () {
-  return this.comments.length;
+PizzaSchema.virtual('commentCount').get(function() {
+  return this.comments.reduce((total, comment) => total + comment.replies.length + 1, 0);
 });
 
 // create the Pizza model using the PizzaSchema
